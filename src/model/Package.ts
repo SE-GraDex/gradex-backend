75fe1ac1d9c0d30bf411a6c9cde399d8ffe836b7
sirawitGOGO <beforever369@gmail.com>
2024-11-20 00:03:48 +0700

    update package

--- src/model/Package.ts
+++ src/model/Package.ts
@@ -2,11 +2,15 @@ import mongoose, { Schema, Document, ObjectId } from 'mongoose';
 
 interface IPackage extends Document {
     user_id: ObjectId,
-    name: string,
+    package_name: 'Basic' | 'Deluxe' | 'Premium',
     price: number,
     features: string,
+    package_start_date: Date,
     package_end_date: Date
 }
+
+// เพิ่ม pakage_start_date โดยเก็บเวลาปัจจุบันหลัง user สมัครแล้วเช็คว่ามี package ก่อนหน้ามั้ย
+// ถ้ามี ให้เอา end date ของ package ก่อนหน้าเป็น start date ของตัวนี้
 
 const packageSchema = new Schema<IPackage>(
     {
@@ -14,8 +18,9 @@ const packageSchema = new Schema<IPackage>(
             type: Schema.Types.ObjectId,
             ref: 'User'
         },
-        name: {
-            type: String, 
+        package_name: {
+            type: String,
+            enum: ['Basic', 'Deluxe', 'Premium'],
             required: true
         },
         price: {
@@ -25,6 +30,10 @@ const packageSchema = new Schema<IPackage>(
         features: {
             type: String, 
             required: true
+        },
+        package_start_date: {
+            type: Date,
+            required: true 
         },
         package_end_date: {
             type: Date,
