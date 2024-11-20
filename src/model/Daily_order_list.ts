ded02c65ad2a873ce820bf24c6125ad6a2ebabd0
Supajit Chantamul <supajit.tttt@gmail.com>
2024-11-20 13:34:55 +0700

    add dailyorder api

--- src/model/Daily_order_list.ts
+++ src/model/Daily_order_list.ts
@@ -3,23 +3,45 @@ import mongoose, { Schema, Document, ObjectId } from "mongoose";
 
 interface IDailyOrderList extends Document {
     date: Date
-    menu: ObjectId
+    menu_title: string,
+    menu_description: string,
+    menu_image: string,
+    ingredient_list: ({
+        name: string,
+        PricePerUnit: number,
+        unit: string,
+        portion: number
+    })[];
 }
 
-const DailyOrderListSchema = new mongoose.Schema<IDailyOrderList>(
+const dailyOrderListSchema = new mongoose.Schema<IDailyOrderList>(
     {
         date: {
             type: Date,
             required: true
         },
-        menu: {
-            type: Schema.Types.ObjectId,
-            ref: 'Menu',
-            required: true,
-        }
+        menu_image: {
+            type: String,
+            required: true
+        },
+        menu_title: {
+            type: String,
+            required: true
+        },
+        menu_description: {
+            type: String,
+            required: true
+        },
+        ingredient_list: [
+            {
+                name: { type: String, required: true },
+                PricePerUnit: { type: Number, required: true },
+                portion: { type: Number, required: true },
+                unit: { type: String, required: true }
+            },
+        ],
     }
 )
 
-const DailyOrderList = mongoose.model<IDailyOrderList>('DailyOrderList', DailyOrderListSchema);
-
+const DailyOrderList = mongoose.model<IDailyOrderList>('DailyOrderList', dailyOrderListSchema);
 export default DailyOrderList;
